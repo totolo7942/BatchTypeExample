59f28ee299e5a5b35f1acee116ab36aaa861eb62
백태성(Taesung Baek)/쿠폰/정산개발팀/11ST <aaron.baek@sk.com>
2022-03-21 11:08:58 +0900

    init

---
+++ src/main/java/com/example/batchtypeexample/BatchTypeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchtypeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTypeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchTypeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/batchtypeexample/BatchTypeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchtypeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTypeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
