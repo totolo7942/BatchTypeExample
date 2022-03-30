ed8f82037055cbd6aba0a26f86d95eb008661b45
백태성(Taesung Baek)/쿠폰/정산개발팀/11ST <aaron.baek@sk.com>
2022-03-30 17:19:10 +0900

    add: URL 분리하는 방법을 spcok도 익힐겸 테스트 추가

--- src/test/groovy/HelloSpockSpec.groovy
+++ src/test/groovy/HelloSpockSpec.groovy
@@ -1,6 +1,11 @@
+import org.apache.commons.validator.routines.UrlValidator
 import org.openjdk.jol.info.ClassLayout
 import org.openjdk.jol.info.GraphLayout
+import org.springframework.web.client.HttpClientErrorException
 import spock.lang.Specification
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class HelloSpockSpec extends Specification {
 
@@ -25,6 +30,66 @@ class HelloSpockSpec extends Specification {
         System.out.println(ClassLayout.parseInstance(test).toString());
 
         System.out.println(GraphLayout.parseInstance(test).toFootprint());
+    }
+
+    def "Url split"() {
+        expect:
+        String url="https://www.11st.co.kr/test"
+        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:\\d*)?(.*)?");
+        Matcher matcher = pattern.matcher(url);
+        matcher.find();
+        String protocol = matcher.group(1);
+        String domain = matcher.group(2);
+        String port = matcher.group(3);
+        String uri = matcher.group(4);
+        System.out.println(url);
+        System.out.println("protocol: " + (protocol != null ? protocol : ""));
+        System.out.println("domain: " + (domain != null ? domain : ""));
+        System.out.println("port: " + (port != null ? port : ""));
+        System.out.println("uri: " + (uri != null ? uri : ""));
+        System.out.println();
+
+        //2번째 방식 URL로 분리
+        UrlValidator urlValidator = new UrlValidator();
+        if(urlValidator.isValid(url)) {
+            URL urls = new URL(url);
+            System.out.println("protocol: " + urls.getProtocol());
+            System.out.println("domain: " + urls.getHost());
+            System.out.println("port: " + urls.getPort());
+            System.out.println("uri: " + urls.getPath());
+        } else {
+            throw new HttpClientErrorException.BadRequest("URL is Not Valid");
+        }
+    }
+
+    def "URL을 정규표현식으로 분리하자"(){
+        given:
+        def url ="https://www.11st.co.kr/test"
+
+        when:
+        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:\\d*)?(.*)?");
+        Matcher matcher = pattern.matcher(url);
+        matcher.find();
+        def domain = matcher.group(2);
+
+        then:
+        println(domain)
+        domain.contains("www.11st.co.kr")
+    }
+
+    def "URL을 사용하여 간단하게 domain을 분리하자" (){
+        given:
+        def url ="https://www.11st.co.kr/test"
+
+        when:
+        URL urls = new URL(url);
+        def domain =  urls.getHost();
+
+        then:
+        UrlValidator urlValidator = new UrlValidator();
+        urlValidator.isValid(url)
+        domain.contains("www.11st.co.kr")
+
     }
 
 }
