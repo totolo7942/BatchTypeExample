c603efd46a4aaab9cba99aaa690b695ecf4855a1
백태성(Taesung Baek)/쿠폰/정산개발팀/11ST <aaron.baek@sk.com>
2022-03-31 00:40:28 +0900

    fix: spock [gradle-intellij idea mode 해결 버전

--- src/test/groovy/HelloSpockSpec.groovy
+++ src/test/groovy/HelloSpockSpec.groovy
@@ -3,6 +3,7 @@ import org.openjdk.jol.info.ClassLayout
 import org.openjdk.jol.info.GraphLayout
 import org.springframework.web.client.HttpClientErrorException
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -62,19 +63,24 @@ class HelloSpockSpec extends Specification {
         }
     }
 
+    @Unroll //반복구문에서 fail일 경우 리포팅을 제대로 해줌, 단일은 효과 없음
     def "URL을 정규표현식으로 분리하자"(){
-        given:
-        def url ="https://www.11st.co.kr/test"
 
-        when:
-        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:\\d*)?(.*)?");
-        Matcher matcher = pattern.matcher(url);
-        matcher.find();
-        def domain = matcher.group(2);
+        expect: //when-then
+        Pattern pattern = Pattern.compile("(https?://)([^:^/]*)(:\\d*)?(.*)?")
+        Matcher matcher = pattern.matcher(url)
 
-        then:
-        println(domain)
-        domain.contains("www.11st.co.kr")
+        matcher.find()
+        def domain = matcher.group(2)
+        domain.contains("11st.co.kr")
+
+        /**
+         * Data Table은 최소 2개의 값을 가지고 있어야됨
+         * 만약 단일로 할경우 "_"를 사용해서 표시할수 있음 https://spockframework.org/spock/docs/1.0/data_driven_testing.html#DataTable
+         */
+        where:
+        url | _
+        "https://www.11st.co.kr" | _(underline : ignored)
     }
 
     def "URL을 사용하여 간단하게 domain을 분리하자" (){
